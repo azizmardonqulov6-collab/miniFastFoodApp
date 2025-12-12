69b4fe321bad2c5e866ef02e62302cfa8b6894ca
Aziz Mardonqulov <azizmardonqulov6@gmail.com>
2025-12-12 23:30:54 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home"
 import Order from './pages/Order'
 export default function App() {
   return (
-    <div className="w-full flex justify-center ">
+    <div className="w-full flex justify-center bg-white ">
       <div className='App w-[380px] px-6 py-6 flex flex-col gap-4 relative'>
           <BrowserRouter>
             <Routes>
