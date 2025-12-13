12aacddea6084a5accb6c751e1ce444a4e9dccd3
Aziz Mardonqulov <azizmardonqulov6@gmail.com>
2025-12-13 20:25:25 +0500

    fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ import Groups from '../conpanents/Groups.tsx'
 export default function Home() {
   return (
     <div>
+        <h1 className='text-center text-gray-500 pt-2 pb-5'>@fastfood500bot</h1>
         <Header />
         <Swiper />
         <Groups />
