2190510e924aff17fa8d9fa107473f4a3cc2ce3e
Aziz Mardonqulov <azizmardonqulov6@gmail.com>
2025-12-19 22:09:34 +0500

    add static code style functional content at Loader  at App

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,19 @@ import { BrowserRouter , Routes , Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Order from './pages/Order'
 import Map from "./pages/Map"
+import Loader from "./conpanents/Loader"
+import { useState } from "react"
 export default function App() {
+  const [loader , setLoader] = useState<boolean>(true);
+  setTimeout(() =>{
+    setLoader(false)
+    console.log(loader);
+  } , 2000)
+
   return (
     <div className="w-full flex justify-center  bg-white ">
       <div className='App w-[380px] px-2 py-6 flex flex-col gap-4 relative'>
+        <Loader loader={loader} />
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Home  />} />
---
+++ src/conpanents/Loader.tsx
@@ -0,0 +1,14 @@
+import loaderGif from '../assets/loader/foodGif.gif'
+
+export default function Loader({loader} : any) {
+  return (
+    <div className={`select w-full h-screen fixed left-0 z-100 flex  justify-center items-end inset-0 pr-[10px] ${loader ? 'top-0' : 'top-[-100vh]'}`}>
+      <div className="w-[370px] h-full relative bg-white flex justify-center items-center ">
+          <span className='w-[300px] h-[300px] flex flex-col justify-center items-center'>
+            <img className='w-[370px] ' src={loaderGif} alt={loaderGif} />
+            <span className='text-center text-xl'>Yuklanmoqda ...</span>
+          </span>
+      </div>
+    </div>
+  )
+}
