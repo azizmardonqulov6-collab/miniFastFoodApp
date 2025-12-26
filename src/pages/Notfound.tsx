0424aa8888c1b0a5ca7043232effaf877aa6e0f8
Aziz Mardonqulov <azizmardonqulov6@gmail.com>
2025-12-26 23:21:26 +0500

    fix not found

--- src/pages/Notfound.tsx
+++ src/pages/Notfound.tsx
@@ -5,7 +5,7 @@ export default function notFound() {
     return (
         // Not found page
         <div className="notFound w-full h-screen flex flex-col items-start">
-               <SecendHeader name="Savatcha" />
+               <SecendHeader name="Eror 404" />
                 <div className='w-full h-screen flex flex-col grow gap-5 justify-center items-center'>
                     <img className='w-[150px]' src={notFOund} alt={notFOund} />
                     <h2 className='text-xl'>Hech Narsa Topilmadi</h2>
